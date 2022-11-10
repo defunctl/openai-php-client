33c3bbcc76d395aa5799c8b8d81962cfdd618298
Justin Frydman <justin.frydman@gmail.com>
2022-11-10 15:40:16 -0700

    Import class, update return types

--- src/Resources/Completions.php
+++ src/Resources/Completions.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace OpenAI\Resources;
 
 use OpenAI\Contracts\Parallel;
+use OpenAI\Exceptions\ErrorException;
+use OpenAI\Exceptions\TransporterException;
+use OpenAI\Exceptions\UnserializableResponse;
 use OpenAI\Responses\Completions\CreateResponse;
 use OpenAI\ValueObjects\Transporter\Payload;
 
@@ -41,13 +44,17 @@ final class Completions implements Parallel
     }
 
     /**
-     * @return \OpenAI\Responses\Completions\CreateResponse[]
+     * @return CreateResponse[]
+     *
+     * @throws ErrorException
+     * @throws TransporterException
+     * @throws UnserializableResponse
      */
     public function run(): array
     {
         /** @var array<array-key, array{id: string, object: string, created: int, model: string, choices: array<int, array{text: string, index: int, logprobs: array{tokens: array<int, string>, token_logprobs: array<int, float>, top_logprobs: array<int, string>|null, text_offset: array<int, int>}|null, finish_reason: string}>, usage: array{prompt_tokens: int, completion_tokens: int, total_tokens: int}}> $responses */
         $responses = $this->transporter->requestObjects($this->payloads);
 
-        return array_map(static fn (array $result): \OpenAI\Responses\Completions\CreateResponse => CreateResponse::from($result), $responses);
+        return array_map(static fn (array $result): CreateResponse => CreateResponse::from($result), $responses);
     }
 }
